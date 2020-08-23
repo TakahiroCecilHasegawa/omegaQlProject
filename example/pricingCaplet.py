f621bb51119cc41a095215b94e788218e2752ebe
Takahiro Cecil Hasegawa <takahiro.hasegawa@omegapartners.jp>
2020-08-23 20:58:04 +0900

    add caplet

---
+++ example/pricingCaplet.py
@@ -0,0 +1,34 @@
+import QuantLib as ql
+import math
+
+# General Setting
+calendar = ql.Japan()
+settlementDate = ql.Date(28, ql.September, 2018)
+settlementDays = 2
+today = calendar.advance(settlementDate, -settlementDays, ql.Days)
+ql.Settings.instance().setEvaluationDate(today)
+
+# tmp yield curve
+tmpYieldCurve = ql.FlatForward(settlementDate, 
+                                ql.QuoteHandle(ql.SimpleQuote(0.01 /100)),
+                                ql.Actual365Fixed(),
+                                ql.Compounded,
+                                ql.Continuous)
+
+strike  = 0.01 / 100
+forward = 0.01 / 100
+vol     = 0.10
+Notional = 10000.00
+optionType = ql.Option.Call
+optionStartDate = calendar.advance(settlementDate, 6, ql.Months) 
+optionMatulity = calendar.advance(optionStartDate, 6, ql.Months)
+
+capletPrice = ql.blackFormula(optionType, 
+                                strike, 
+                                forward, 
+                                vol * math.sqrt((optionMatulity - optionStartDate) / 360))
+
+discuountFactor = tmpYieldCurve.discount(optionMatulity)
+capletPrice = discuountFactor * capletPrice * Notional
+
+print('capletPrice =', capletPrice)
